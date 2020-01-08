4b8ec8e8d7034189bc9bc11c0184210372b9b49a
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-01-08 12:05:06 +0000

    test: Add with native configuration multiple test

---
+++ src/main/resources/docs/multiple-tests/with-config-file/src/results.kt
@@ -0,0 +1,11 @@
+package hello
+/**
+ *This is a test
+ */
+fun main(args : Array<String>) {
+    val myFloat = 4.0F
+    val myFloat = 1.0F
+    val myDouble = 2.0F
+    println(getHelloString())
+}
+
